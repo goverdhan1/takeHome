0a4251d00232857fe92fae084a30ceb3e26ce8fd
VenkataPraneethT <venkata.tatiraju@aalto.fi>
2017-09-02 20:48:12 +0530

    changes made to css

---
+++ Take Home/Scripts/caseFunc.js
@@ -0,0 +1,14 @@
+﻿
+
+
+/*$(document).ready(function () {
+    $(function () {
+        $("#datetimepicker1").datepicker();
+    });
+}); */
+
+
+$(function () {
+    $("#datepicker1").datepicker();
+    $("#datepicker2").datepicker();
+});
